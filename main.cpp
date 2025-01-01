ee3bc74b40b1f5e502ec3cef4505425271752ee4
Tamim Norani <tamimn@uia.no>
2025-01-01 01:25:56 +0100

    Got auth token, can get liked songs (not all of them)

--- main.cpp
+++ main.cpp
@@ -10,18 +10,39 @@ static size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
     return size * nmemb;
 }
 
-// need to set curl header to null
-struct curl_slist *headerContentType=NULL;
-struct curl_slist *headerAuthToken=NULL;
+
+static std::string base64_encode(const std::string &in) {
+    std::string out;
+    static const std::string base64_chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789+/";
+
+    int val = 0, valb = -6;
+    for (unsigned char c : in) {
+        val = (val << 8) + c;
+        valb += 8;
+        while (valb >= 0) {
+            out.push_back(base64_chars[(val >> valb) & 0x3F]);
+            valb -= 6;
+        }
+    }
+    if (valb > -6) out.push_back(base64_chars[((val << 8) >> (valb + 8)) & 0x3F]);
+    while (out.size() % 4) out.push_back('=');
+    return out;
+}
 
 std::string getAccessToken() {
+
+    struct curl_slist *header = NULL;
+
     CURL *curl;
     CURLcode res;
     std::string readBuffer;
     std::string accessToken;
 
     // making our header
-    headerContentType = curl_slist_append(NULL, "Content-Type: application/x-www-form-urlencoded");
+    header = curl_slist_append(NULL, "Content-Type: application/x-www-form-urlencoded");
 
     // id and secret
     char *client_id_secret=
@@ -29,13 +50,13 @@ std::string getAccessToken() {
         "&client_id=a24ea60b20244fd299abb97fc9ce2ce5"
         "&client_secret=dd43497448614f0dbd9469726b50f673";
 
-    // initializing curl > if successfull
+
     curl = curl_easy_init();
     if(curl) {
 
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, client_id_secret);
         curl_easy_setopt(curl, CURLOPT_URL, "https://accounts.spotify.com/api/token");
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerContentType);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header);
 
         // uses our function to process the data
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &write_data);
@@ -50,87 +71,84 @@ std::string getAccessToken() {
 
         return accessToken;
     }
+
 }
 
 
 std::string getLikedSongs(std::string accessToken) {
+
+    struct curl_slist *header = NULL;
+
     CURL *curl;
     CURLcode res;
     std::string LikedSongsData;
 
-    std::string AuthBearer = "Authorization: Bearer ";
-    std::string authBearerToken = AuthBearer + accessToken;
 
+
+    std::string GrantType = "Authorization: Bearer ";
+    std::string authBearerToken = GrantType + accessToken;
 
     // making our header
-    headerAuthToken = curl_slist_append(NULL, authBearerToken.c_str());
+    header = curl_slist_append(NULL, authBearerToken.c_str());
 
-
-    // initializing curl > if successfull
     curl = curl_easy_init();
     if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.spotify.com/v1/me");
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header);
 
-        curl_easy_setopt(curl, CURLOPT_URL, "https://api.spotify.com/v1/me/tracks");
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerAuthToken);
-
-
-        // uses our function to process the data
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &write_data);
-        // stores the data in readBuffer
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &LikedSongsData);
 
-        // actually performs the request
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
-
 
         return LikedSongsData;
     }
 }
 
 std::string getPlaylistData(std::string accessToken) {
+
+    struct curl_slist *header = NULL;
+
     CURL *curl;
     CURLcode res;
     std::string playlistData;
 
-    std::string AuthBearer = "Authorization: Bearer ";
-    std::string authBearerToken = AuthBearer + accessToken;
+    std::string GrantType = "Authorization: Bearer ";
+    std::string authBearerToken = GrantType + accessToken;
 
 
     // making our header
-    headerAuthToken = curl_slist_append(NULL, authBearerToken.c_str());
+    header = curl_slist_append(NULL, authBearerToken.c_str());
 
 
-    // initializing curl > if successfull
+
     curl = curl_easy_init();
     if(curl) {
         curl_easy_setopt(curl, CURLOPT_URL, "https://api.spotify.com/v1/playlists/09uKR1njKVo82C7vTpnQXu");
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerAuthToken);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header);
 
-        // uses our function to process the data
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &write_data);
-        // stores the data in readBuffer
+
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &playlistData);
 
-        // actually performs the request
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
 
-
         return playlistData;
     }
+
 }
 
-void webserver() {
-    
+std::string webserver() {
+
     std::string client_id = "a24ea60b20244fd299abb97fc9ce2ce5";
     std::string redirect_uri = "http://localhost:8888/callback";
-    std::string scope = "user-read-private user-read-email";
+    std::string scope = "user-read-private user-read-email user-library-read";
 
     // creating the server
     AuthServer server(U("http://localhost:8888/callback"));
     server.start();
-    std::cout << "Server started and listening..." << std::endl;
 
     // building the url
     std::string auth_url =
@@ -147,34 +165,79 @@ void webserver() {
     std::cin.get();
 
     // after user presses enter
-    auto received_code = server.get_auth_code();
-    if (!received_code.empty()) {
-        std::cout << "auth code: " << utility::conversions::to_utf8string(received_code) << std::endl;
-    } else {
+    auto received_oAuth_code = server.get_auth_code();
+    if (received_oAuth_code.empty()) {
         std::cout << "didnt get a code" << std::endl;
     }
 
     server.stop();
+
+    return received_oAuth_code;
+}
+
+std::string oAuth_to_token(std::string oAuth_code) {
+    CURL *curl;
+    CURLcode res;
+    std::string readBuffer;
+    std::string oAuth_Token;
+
+
+
+    // making our headers
+    std::string client_id = "a24ea60b20244fd299abb97fc9ce2ce5";
+    std::string client_secret = "dd43497448614f0dbd9469726b50f673";
+    std::string credentials = client_id + ":" + client_secret;
+
+    // Create the Authorization header with Basic auth
+    std::string auth_header = "Authorization: Basic " + base64_encode(credentials);
+
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, auth_header.c_str());
+    headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
+
+
+    // body parameters
+    std::string body_parameters=
+        "grant_type=authorization_code"
+        "&code=" + oAuth_code +
+        "&redirect_uri=http://localhost:8888/callback";
+
+
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, body_parameters.c_str());
+        curl_easy_setopt(curl, CURLOPT_URL, "https://accounts.spotify.com/api/token");
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+        // uses our function to process the data
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &write_data);
+        // stores the data in readBuffer
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+
+        // actually performs the request
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+
+        // extract the token from readbuffer
+        //std::cout << "Response: " << readBuffer << std::endl;
+        oAuth_Token = readBuffer.substr(17, 278);
+
+        return oAuth_Token;
+    }
 }
 
 int main()
 {
+    //gets oauth code using webserver func, passes that into oAuth to token, stores that token in oAuthToken
+    std::string oAuthToken = oAuth_to_token(webserver());
 
-    webserver();
+    // pass the auth token to these various funtions
+    std::string playlistData = getPlaylistData(oAuthToken);
+    std::string likedSongs = getLikedSongs(oAuthToken);
 
-    std::string accessToken = getAccessToken();
-    std::string playlistData = getPlaylistData(accessToken);
-    std::string likedSongs = getLikedSongs(accessToken);
-
-
-
-
-    //std::cout << accessToken << std::endl;
-    //std::cout << playlistData << std::endl;
-    //std::cout << likedSongs << std::endl;
-
-
-
+    // Za prints
+    std::cout << "playlist data" << playlistData << std::endl;
+    std::cout << "liked songs" << likedSongs << std::endl;
 
     return 0;
 }
